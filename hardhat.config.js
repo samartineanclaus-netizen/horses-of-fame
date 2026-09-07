82cbb90566762e7e79fa4838400af0cda4910945
samartineanclaus-netizen <samartineanclaus@gmail.com>
2026-09-07 16:11:33 +0300

    Update Solidity compiler

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ const PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
 
 module.exports = {
   solidity: {
-    version: "0.8.24",
+    version: "0.8.25",
     settings: {
       optimizer: {
         enabled: true,
