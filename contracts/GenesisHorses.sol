a2e027b502332931dd0c9e5d765d3e1a8e158af2
samartineanclaus-netizen <samartineanclaus@gmail.com>
2026-09-07 17:49:04 +0300

    Update GenesisHorses.sol

--- contracts/GenesisHorses.sol
+++ contracts/GenesisHorses.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract GenesisHorses is ERC721Enumerable, Ownable, Pausable {
+
+    // =============================================================
+    //                         SUPPLY
+    // =============================================================
+
     uint256 public constant MAX_SUPPLY = 2222;
+
     uint256 public constant HALL_OF_FAME_SUPPLY = 22;
     uint256 public constant VOTING_SUPPLY = 2200;
 
@@ -16,12 +22,28 @@ contract GenesisHorses is ERC721Enumerable, Ownable, Pausable {
     uint256 public constant EPIC_SUPPLY = 240;
     uint256 public constant LEGENDARY_SUPPLY = 190;
 
+    // =============================================================
+    //                         MINT
+    // =============================================================
+
     uint256 public nextTokenId = 1;
+
+    // TESTNET ONLY.
+    // Final mainnet mint model will follow the approved launch model.
     uint256 public testMintPrice = 0.001 ether;
+
+    // =============================================================
+    //                         METADATA
+    // =============================================================
+
     string private _baseTokenURI;
     string public placeholderURI;
 
     bool public revealed;
+
+    // =============================================================
+    //                         RARITY
+    // =============================================================
 
     enum Rarity {
         Unassigned,
@@ -35,81 +57,100 @@ contract GenesisHorses is ERC721Enumerable, Ownable, Pausable {
 
     mapping(uint256 => Rarity) private _rarity;
 
-    constructor(
-        string memory initialPlaceholderURI
-    )
+    // =============================================================
+    //                         EVENTS
+    // =============================================================
+
+    event TestMint(address indexed minter, uint256 quantity, uint256 value);
+    event RarityAssigned(uint256 indexed tokenId, Rarity rarity);
+    event CollectionRevealed(string baseURI);
+
+    // =============================================================
+    //                         CONSTRUCTOR
+    // =============================================================
+
+    constructor(string memory initialPlaceholderURI)
         ERC721("Horses of Fame - Genesis", "HOFGEN")
         Ownable(msg.sender)
     {
         placeholderURI = initialPlaceholderURI;
     }
 
- function ownerMint(address to, uint256 quantity)
-    external
-    onlyOwner
-    whenNotPaused
-{
-    require(quantity > 0, "Quantity must be greater than zero");
-    require(
-        totalSupply() + quantity <= MAX_SUPPLY,
-        "Genesis supply exceeded"
-    );
+    // =============================================================
+    //                         OWNER MINT
+    // =============================================================
 
-    for (uint256 i = 0; i < quantity; i++) {
-        uint256 tokenId = nextTokenId;
-        nextTokenId++;
+    function ownerMint(address to, uint256 quantity)
+        external
+        onlyOwner
+        whenNotPaused
+    {
+        require(quantity > 0, "Quantity must be greater than zero");
+        require(
+            totalSupply() + quantity <= MAX_SUPPLY,
+            "Genesis supply exceeded"
+        );
 
-        _safeMint(to, tokenId);
+        for (uint256 i = 0; i < quantity; i++) {
+            uint256 tokenId = nextTokenId;
+            nextTokenId++;
+
+            _safeMint(to, tokenId);
+        }
     }
-}
 
-function publicTestMint(uint256 quantity)
-    external
-    payable
-    whenNotPaused
-{
-    require(quantity > 0, "Quantity must be greater than zero");
-    require(
-        totalSupply() + quantity <= MAX_SUPPLY,
-        "Genesis supply exceeded"
-    );
-    require(
-        msg.value == testMintPrice * quantity,
-        "Incorrect test mint payment"
-    );
+    // =============================================================
+    //                         PUBLIC TEST MINT
+    // =============================================================
 
-    for (uint256 i = 0; i < quantity; i++) {
-        uint256 tokenId = nextTokenId;
-        nextTokenId++;
+    function publicTestMint(uint256 quantity)
+        external
+        payable
+        whenNotPaused
+    {
+        require(quantity > 0, "Quantity must be greater than zero");
 
-        _safeMint(msg.sender, tokenId);
+        require(
+            totalSupply() + quantity <= MAX_SUPPLY,
+            "Genesis supply exceeded"
+        );
+
+        require(
+            msg.value == testMintPrice * quantity,
+            "Incorrect test mint payment"
+        );
+
+        for (uint256 i = 0; i < quantity; i++) {
+            uint256 tokenId = nextTokenId;
+            nextTokenId++;
+
+            _safeMint(msg.sender, tokenId);
+        }
+
+        emit TestMint(msg.sender, quantity, msg.value);
     }
-}
 
-function setTestMintPrice(uint256 newPrice)
-    external
-    onlyOwner
-{
-    testMintPrice = newPrice;
-}
+    function setTestMintPrice(uint256 newPrice)
+        external
+        onlyOwner
+    {
+        testMintPrice = newPrice;
+    }
 
-function withdrawTestFunds()
-    external
-    onlyOwner
-{
-    payable(owner()).transfer(address(this).balance);
-}
-        function withdrawTestFunds()
-    external
-    onlyOwner
-{
-    payable(owner()).transfer(address(this).balance);
-}
+    function withdrawTestFunds()
+        external
+        onlyOwner
+    {
+        uint256 balance = address(this).balance;
 
-function setRarity(
-    uint256 tokenId,
-    Rarity rarity_
-)
+        (bool success, ) = payable(owner()).call{value: balance}("");
+
+        require(success, "Withdraw failed");
+    }
+
+    // =============================================================
+    //                         RARITY
+    // =============================================================
 
     function setRarity(
         uint256 tokenId,
@@ -122,6 +163,8 @@ function setRarity(
         require(rarity_ != Rarity.Unassigned, "Invalid rarity");
 
         _rarity[tokenId] = rarity_;
+
+        emit RarityAssigned(tokenId, rarity_);
     }
 
     function rarityOf(uint256 tokenId)
@@ -130,8 +173,13 @@ function setRarity(
         returns (Rarity)
     {
         require(_ownerOf(tokenId) != address(0), "Token does not exist");
+
         return _rarity[tokenId];
     }
+
+    // =============================================================
+    //                         VOTING POWER
+    // =============================================================
 
     function votingPowerOf(uint256 tokenId)
         public
@@ -142,15 +190,34 @@ function setRarity(
 
         Rarity rarity_ = _rarity[tokenId];
 
-        if (rarity_ == Rarity.Common) return 1;
-        if (rarity_ == Rarity.Uncommon) return 2;
-        if (rarity_ == Rarity.Rare) return 3;
-        if (rarity_ == Rarity.Epic) return 4;
-        if (rarity_ == Rarity.Legendary) return 5;
+        if (rarity_ == Rarity.Common) {
+            return 1;
+        }
 
-        // Hall of Fame horses have 0 VP.
+        if (rarity_ == Rarity.Uncommon) {
+            return 2;
+        }
+
+        if (rarity_ == Rarity.Rare) {
+            return 3;
+        }
+
+        if (rarity_ == Rarity.Epic) {
+            return 4;
+        }
+
+        if (rarity_ == Rarity.Legendary) {
+            return 5;
+        }
+
+        // Hall of Fame = 0 VP
+        // Unassigned = 0 VP
         return 0;
     }
+
+    // =============================================================
+    //                         METADATA / REVEAL
+    // =============================================================
 
     function setPlaceholderURI(string calldata newPlaceholderURI)
         external
@@ -168,6 +235,8 @@ function setRarity(
 
         _baseTokenURI = newBaseURI;
         revealed = true;
+
+        emit CollectionRevealed(newBaseURI);
     }
 
     function tokenURI(uint256 tokenId)
@@ -194,11 +263,21 @@ function setRarity(
         return _baseTokenURI;
     }
 
-    function pause() external onlyOwner {
+    // =============================================================
+    //                         PAUSE
+    // =============================================================
+
+    function pause()
+        external
+        onlyOwner
+    {
         _pause();
     }
 
-    function unpause() external onlyOwner {
+    function unpause()
+        external
+        onlyOwner
+    {
         _unpause();
     }
 }
