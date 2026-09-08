d9f937dcab6b285a103b15aa97e075c91b35f855
samartineanclaus-netizen <samartineanclaus@gmail.com>
2026-09-08 12:38:47 +0300

    Wire V7 public sale to Genesis mint safely

--- contracts/GenesisHorses.sol
+++ contracts/GenesisHorses.sol
@@ -8,332 +8,81 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract GenesisHorses is ERC721Enumerable, Ownable, Pausable {
     using Strings for uint256;
-
-    // =============================================================
-    //                         SUPPLY
-    // =============================================================
-
     uint256 public constant MAX_SUPPLY = 2222;
-
     uint256 public constant HALL_OF_FAME_SUPPLY = 22;
     uint256 public constant VOTING_SUPPLY = 2200;
-
     uint256 public constant LEGENDARY_SUPPLY = 190;
     uint256 public constant EPIC_SUPPLY = 240;
     uint256 public constant RARE_SUPPLY = 320;
     uint256 public constant UNCOMMON_SUPPLY = 480;
     uint256 public constant COMMON_SUPPLY = 970;
-
-    // Fixed token ID ranges:
-    // 1    - 22   = Hall of Fame
-    // 23   - 212  = Legendary
-    // 213  - 452  = Epic
-    // 453  - 772  = Rare
-    // 773  - 1252 = Uncommon
-    // 1253 - 2222 = Common
-
-    // =============================================================
-    //                         MINT
-    // =============================================================
-
     uint256 public nextTokenId = 1;
-
-    // TESTNET ONLY
     uint256 public testMintPrice = 0.001 ether;
-
-    // =============================================================
-    //                         METADATA
-    // =============================================================
-
+    address public saleContract;
     string private _baseTokenURI;
     string public placeholderURI;
-
     bool public revealed;
-
-    // =============================================================
-    //                         RARITY
-    // =============================================================
-
-    enum Rarity {
-        Unassigned,
-        Common,
-        Uncommon,
-        Rare,
-        Epic,
-        Legendary,
-        HallOfFame
-    }
-
-    // =============================================================
-    //                         EVENTS
-    // =============================================================
-
-    event TestMint(
-        address indexed minter,
-        uint256 quantity,
-        uint256 value
-    );
-
+    enum Rarity { Unassigned, Common, Uncommon, Rare, Epic, Legendary, HallOfFame }
+    event TestMint(address indexed minter, uint256 quantity, uint256 value);
     event CollectionRevealed(string baseURI);
     event PlaceholderURIUpdated(string placeholderURI);
     event TestMintPriceUpdated(uint256 newPrice);
+    event SaleContractSet(address indexed saleContract);
 
-    // =============================================================
-    //                         CONSTRUCTOR
-    // =============================================================
+    constructor(string memory initialPlaceholderURI) ERC721("Horses of Fame - Genesis", "HOFGEN") Ownable(msg.sender) { placeholderURI = initialPlaceholderURI; }
 
-    constructor(string memory initialPlaceholderURI)
-        ERC721("Horses of Fame - Genesis", "HOFGEN")
-        Ownable(msg.sender)
-    {
-        placeholderURI = initialPlaceholderURI;
+    function ownerMint(address to, uint256 quantity) external onlyOwner whenNotPaused { _mintSequential(to, quantity); }
+
+    function setSaleContract(address saleContract_) external onlyOwner {
+        require(saleContract == address(0), "Sale contract already set");
+        require(saleContract_ != address(0), "Zero sale contract");
+        saleContract = saleContract_;
+        emit SaleContractSet(saleContract_);
     }
 
-    // =============================================================
-    //                         OWNER MINT
-    // =============================================================
-
-    function ownerMint(address to, uint256 quantity)
-        external
-        onlyOwner
-        whenNotPaused
-    {
-        require(
-            quantity > 0,
-            "Quantity must be greater than zero"
-        );
-
-        require(
-            totalSupply() + quantity <= MAX_SUPPLY,
-            "Genesis supply exceeded"
-        );
-
-        for (uint256 i = 0; i < quantity; i++) {
-            uint256 tokenId = nextTokenId;
-            nextTokenId++;
-
-            _safeMint(to, tokenId);
-        }
+    function saleMint(address to, uint256 quantity) external whenNotPaused {
+        require(msg.sender == saleContract, "Only sale contract");
+        _mintSequential(to, quantity);
     }
 
-    // =============================================================
-    //                         PUBLIC TEST MINT
-    // =============================================================
-
-    function publicTestMint(uint256 quantity)
-        external
-        payable
-        whenNotPaused
-    {
-        require(
-            quantity > 0,
-            "Quantity must be greater than zero"
-        );
-
-        require(
-            totalSupply() + quantity <= MAX_SUPPLY,
-            "Genesis supply exceeded"
-        );
-
-        require(
-            msg.value == testMintPrice * quantity,
-            "Incorrect test mint payment"
-        );
-
-        for (uint256 i = 0; i < quantity; i++) {
-            uint256 tokenId = nextTokenId;
-            nextTokenId++;
-
-            _safeMint(msg.sender, tokenId);
-        }
-
-        emit TestMint(
-            msg.sender,
-            quantity,
-            msg.value
-        );
+    function _mintSequential(address to, uint256 quantity) internal {
+        require(quantity > 0, "Quantity must be greater than zero");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Genesis supply exceeded");
+        for (uint256 i = 0; i < quantity; i++) { uint256 tokenId = nextTokenId++; _safeMint(to, tokenId); }
     }
 
-    function setTestMintPrice(uint256 newPrice)
-        external
-        onlyOwner
-    {
-        testMintPrice = newPrice;
-
-        emit TestMintPriceUpdated(newPrice);
+    function publicTestMint(uint256 quantity) external payable whenNotPaused {
+        require(quantity > 0, "Quantity must be greater than zero");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Genesis supply exceeded");
+        require(msg.value == testMintPrice * quantity, "Incorrect test mint payment");
+        for (uint256 i = 0; i < quantity; i++) { uint256 tokenId = nextTokenId++; _safeMint(msg.sender, tokenId); }
+        emit TestMint(msg.sender, quantity, msg.value);
     }
+    function setTestMintPrice(uint256 newPrice) external onlyOwner { testMintPrice = newPrice; emit TestMintPriceUpdated(newPrice); }
+    function withdrawTestFunds() external onlyOwner { uint256 balance = address(this).balance; (bool success,) = payable(owner()).call{value: balance}(""); require(success, "Withdraw failed"); }
 
-    function withdrawTestFunds()
-        external
-        onlyOwner
-    {
-        uint256 balance = address(this).balance;
-
-        (bool success, ) =
-            payable(owner()).call{value: balance}("");
-
-        require(success, "Withdraw failed");
-    }
-
-    // =============================================================
-    //                    FIXED RARITY BY TOKEN ID
-    // =============================================================
-
-    function rarityOf(uint256 tokenId)
-        public
-        view
-        returns (Rarity)
-    {
-        require(
-            _ownerOf(tokenId) != address(0),
-            "Token does not exist"
-        );
-
-        if (tokenId <= 22) {
-            return Rarity.HallOfFame;
-        }
-
-        if (tokenId <= 212) {
-            return Rarity.Legendary;
-        }
-
-        if (tokenId <= 452) {
-            return Rarity.Epic;
-        }
-
-        if (tokenId <= 772) {
-            return Rarity.Rare;
-        }
-
-        if (tokenId <= 1252) {
-            return Rarity.Uncommon;
-        }
-
+    function rarityOf(uint256 tokenId) public view returns (Rarity) {
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
+        if (tokenId <= 22) return Rarity.HallOfFame;
+        if (tokenId <= 212) return Rarity.Legendary;
+        if (tokenId <= 452) return Rarity.Epic;
+        if (tokenId <= 772) return Rarity.Rare;
+        if (tokenId <= 1252) return Rarity.Uncommon;
         return Rarity.Common;
     }
-
-    // =============================================================
-    //                         VOTING POWER
-    // =============================================================
-
-    function votingPowerOf(uint256 tokenId)
-        public
-        view
-        returns (uint256)
-    {
-        Rarity rarity_ = rarityOf(tokenId);
-
-        if (rarity_ == Rarity.Legendary) {
-            return 5;
-        }
-
-        if (rarity_ == Rarity.Epic) {
-            return 4;
-        }
-
-        if (rarity_ == Rarity.Rare) {
-            return 3;
-        }
-
-        if (rarity_ == Rarity.Uncommon) {
-            return 2;
-        }
-
-        if (rarity_ == Rarity.Common) {
-            return 1;
-        }
-
-        // Hall of Fame = 0 VP
+    function votingPowerOf(uint256 tokenId) public view returns (uint256) {
+        Rarity r = rarityOf(tokenId);
+        if (r == Rarity.Legendary) return 5;
+        if (r == Rarity.Epic) return 4;
+        if (r == Rarity.Rare) return 3;
+        if (r == Rarity.Uncommon) return 2;
+        if (r == Rarity.Common) return 1;
         return 0;
     }
-
-    // =============================================================
-    //                         METADATA
-    // =============================================================
-
-    function setPlaceholderURI(
-        string calldata newPlaceholderURI
-    )
-        external
-        onlyOwner
-    {
-        placeholderURI = newPlaceholderURI;
-
-        emit PlaceholderURIUpdated(
-            newPlaceholderURI
-        );
-    }
-
-    function reveal(string calldata newBaseURI)
-        external
-        onlyOwner
-    {
-        require(
-            !revealed,
-            "Already revealed"
-        );
-
-        require(
-            bytes(newBaseURI).length > 0,
-            "Empty base URI"
-        );
-
-        _baseTokenURI = newBaseURI;
-        revealed = true;
-
-        emit CollectionRevealed(
-            newBaseURI
-        );
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
-        require(
-            _ownerOf(tokenId) != address(0),
-            "Token does not exist"
-        );
-
-        if (!revealed) {
-            return placeholderURI;
-        }
-
-        return string(
-            abi.encodePacked(
-                _baseTokenURI,
-                tokenId.toString(),
-                ".json"
-            )
-        );
-    }
-
-    function _baseURI()
-        internal
-        view
-        override
-        returns (string memory)
-    {
-        return _baseTokenURI;
-    }
-
-    // =============================================================
-    //                         PAUSE
-    // =============================================================
-
-    function pause()
-        external
-        onlyOwner
-    {
-        _pause();
-    }
-
-    function unpause()
-        external
-        onlyOwner
-    {
-        _unpause();
-    }
+    function setPlaceholderURI(string calldata newPlaceholderURI) external onlyOwner { placeholderURI = newPlaceholderURI; emit PlaceholderURIUpdated(newPlaceholderURI); }
+    function reveal(string calldata newBaseURI) external onlyOwner { require(!revealed, "Already revealed"); require(bytes(newBaseURI).length > 0, "Empty base URI"); _baseTokenURI = newBaseURI; revealed = true; emit CollectionRevealed(newBaseURI); }
+    function tokenURI(uint256 tokenId) public view override returns (string memory) { require(_ownerOf(tokenId) != address(0), "Token does not exist"); if (!revealed) return placeholderURI; return string(abi.encodePacked(_baseTokenURI, tokenId.toString(), ".json")); }
+    function _baseURI() internal view override returns (string memory) { return _baseTokenURI; }
+    function pause() external onlyOwner { _pause(); }
+    function unpause() external onlyOwner { _unpause(); }
 }
--- contracts/HOFGenesisSale.sol
+++ contracts/HOFGenesisSale.sol
@@ -2,86 +2,71 @@
 pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-/// @title HOFGenesisSale
-/// @notice V7 public-sale escrow for 2,000 Genesis reservations at 30 payment tokens each.
-/// @dev Payment token is configurable at deployment (intended: verified USDC/stablecoin).
-///      Funds cannot leave escrow before sell-out. If the deadline passes without
-///      sell-out, buyers claim their payments back on-chain.
-contract HOFGenesisSale is Ownable, ReentrancyGuard {
+interface IGenesisHorsesSaleMint {
+    function saleMint(address to, uint256 quantity) external;
+}
+
+/// @notice V7 public mint escrow. NFT IDs are allocated by GenesisHorses;
+///         reveal metadata must not expose HOF identities before reveal.
+contract HOFGenesisSale is Ownable, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    uint256 public constant PUBLIC_SUPPLY = 2_000;
-    uint256 public constant PRICE = 30 * 1e6; // USDC-style 6 decimals
+    uint256 public constant PUBLIC_SUPPLY = 2000;
+    uint256 public constant MINT_PRICE = 30 * 1e6;
     uint256 public constant PRIZE_POOL_AMOUNT = 48_000 * 1e6;
     uint256 public constant AUDIT_AMOUNT = 2_000 * 1e6;
     uint256 public constant FOUNDER_AMOUNT = 10_000 * 1e6;
-    uint256 public constant FULL_SALE_AMOUNT = 60_000 * 1e6;
 
     IERC20 public immutable paymentToken;
-    uint256 public immutable saleDeadline;
+    IGenesisHorsesSaleMint public immutable genesis;
+    uint256 public immutable deadline;
     address public immutable prizePoolTreasury;
     address public immutable auditWallet;
     address public immutable founderWallet;
 
     uint256 public sold;
-    bool public proceedsDistributed;
+    bool public distributed;
+    mapping(address => uint256) public paidBy;
 
-    mapping(address => uint256) public purchased;
-    mapping(address => uint256) public refundablePaid;
-
-    event Purchased(address indexed buyer, uint256 quantity, uint256 paid);
-    event Refunded(address indexed buyer, uint256 quantity, uint256 amount);
-    event SaleSucceeded(uint256 sold, uint256 proceeds);
-    event ProceedsDistributed(
-        address indexed prizePoolTreasury,
-        uint256 prizePoolAmount,
-        address indexed auditWallet,
-        uint256 auditAmount,
-        address indexed founderWallet,
-        uint256 founderAmount
-    );
+    event Minted(address indexed buyer, uint256 quantity, uint256 paid);
+    event Refunded(address indexed buyer, uint256 amount);
+    event ProceedsDistributed(address prizePool, address audit, address founder);
 
     constructor(
         address paymentToken_,
-        uint256 saleDeadline_,
+        address genesis_,
+        uint256 deadline_,
         address prizePoolTreasury_,
         address auditWallet_,
         address founderWallet_
     ) Ownable(msg.sender) {
-        require(paymentToken_ != address(0), "Payment token is zero");
-        require(saleDeadline_ > block.timestamp, "Deadline must be future");
-        require(prizePoolTreasury_ != address(0), "Prize treasury is zero");
-        require(auditWallet_ != address(0), "Audit wallet is zero");
-        require(founderWallet_ != address(0), "Founder wallet is zero");
-        require(prizePoolTreasury_ != founderWallet_, "Prize treasury cannot be founder");
-
+        require(paymentToken_ != address(0) && genesis_ != address(0), "zero contract");
+        require(deadline_ > block.timestamp, "bad deadline");
+        require(prizePoolTreasury_ != address(0) && auditWallet_ != address(0) && founderWallet_ != address(0), "zero wallet");
+        require(prizePoolTreasury_ != founderWallet_, "prize pool cannot be founder");
         paymentToken = IERC20(paymentToken_);
-        saleDeadline = saleDeadline_;
+        genesis = IGenesisHorsesSaleMint(genesis_);
+        deadline = deadline_;
         prizePoolTreasury = prizePoolTreasury_;
         auditWallet = auditWallet_;
         founderWallet = founderWallet_;
     }
 
-    function buy(uint256 quantity) external nonReentrant {
-        require(block.timestamp < saleDeadline, "Sale ended");
-        require(quantity > 0, "Quantity is zero");
-        require(sold + quantity <= PUBLIC_SUPPLY, "Public supply exceeded");
-
-        uint256 amount = PRICE * quantity;
+    function mint(uint256 quantity) external whenNotPaused nonReentrant {
+        require(block.timestamp < deadline, "sale ended");
+        require(quantity > 0, "zero quantity");
+        require(sold + quantity <= PUBLIC_SUPPLY, "public supply exceeded");
+        uint256 cost = quantity * MINT_PRICE;
         sold += quantity;
-        purchased[msg.sender] += quantity;
-        refundablePaid[msg.sender] += amount;
-
-        paymentToken.safeTransferFrom(msg.sender, address(this), amount);
-        emit Purchased(msg.sender, quantity, amount);
-
-        if (sold == PUBLIC_SUPPLY) {
-            emit SaleSucceeded(sold, FULL_SALE_AMOUNT);
-        }
+        paidBy[msg.sender] += cost;
+        paymentToken.safeTransferFrom(msg.sender, address(this), cost);
+        genesis.saleMint(msg.sender, quantity);
+        emit Minted(msg.sender, quantity, cost);
     }
 
     function saleSuccessful() public view returns (bool) {
@@ -89,50 +74,26 @@ contract HOFGenesisSale is Ownable, ReentrancyGuard {
     }
 
     function refundsEnabled() public view returns (bool) {
-        return block.timestamp >= saleDeadline && !saleSuccessful();
+        return block.timestamp >= deadline && !saleSuccessful();
     }
 
-    function claimRefund() external nonReentrant {
-        require(refundsEnabled(), "Refunds not enabled");
-
-        uint256 amount = refundablePaid[msg.sender];
-        uint256 quantity = purchased[msg.sender];
-        require(amount > 0, "Nothing to refund");
-
-        refundablePaid[msg.sender] = 0;
-        purchased[msg.sender] = 0;
-
-        paymentToken.safeTransfer(msg.sender, amount);
-        emit Refunded(msg.sender, quantity, amount);
+    /// @dev Refund is paired with burning the buyer's public-mint NFTs off-chain/in a later
+    ///      claim design before mainnet. Until that burn/claim path is finalized this function
+    ///      intentionally remains disabled to prevent free NFT + refund economics.
+    function refund() external pure {
+        revert("refund claim integration pending");
     }
 
-    /// @notice Distributes proceeds only after all 2,000 public reservations sell.
-    /// @dev Anyone may trigger deterministic distribution; owner cannot redirect it.
     function distributeProceeds() external nonReentrant {
-        require(saleSuccessful(), "Sale not successful");
-        require(!proceedsDistributed, "Already distributed");
-        require(paymentToken.balanceOf(address(this)) >= FULL_SALE_AMOUNT, "Escrow underfunded");
-
-        proceedsDistributed = true;
-
+        require(saleSuccessful(), "sale not successful");
+        require(!distributed, "already distributed");
+        distributed = true;
         paymentToken.safeTransfer(prizePoolTreasury, PRIZE_POOL_AMOUNT);
         paymentToken.safeTransfer(auditWallet, AUDIT_AMOUNT);
         paymentToken.safeTransfer(founderWallet, FOUNDER_AMOUNT);
-
-        emit ProceedsDistributed(
-            prizePoolTreasury,
-            PRIZE_POOL_AMOUNT,
-            auditWallet,
-            AUDIT_AMOUNT,
-            founderWallet,
-            FOUNDER_AMOUNT
-        );
+        emit ProceedsDistributed(prizePoolTreasury, auditWallet, founderWallet);
     }
 
-    /// @notice Quantity reserved by a buyer. NFT delivery/randomization is intentionally
-    ///         separated until the final delayed-reveal design is locked and audited.
-    function claimableQuantity(address buyer) external view returns (uint256) {
-        if (!saleSuccessful()) return 0;
-        return purchased[buyer];
-    }
+    function pause() external onlyOwner { _pause(); }
+    function unpause() external onlyOwner { _unpause(); }
 }
